b520f138e82e642233ce963ef20ad2e6cc68a9c9
Carlos Fabian Salgado Maldonado <carlos.salgado09@hotmail.com>
2019-11-24 22:55:11 -0600

    FUNERARIA

---
+++ BLL/Ataud.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+
+namespace BLL
+{
+    public class AtaudBLL
+    {
+        public ATAUD CrearAtaud(ATAUD Ataud) 
+        {
+            ATAUD nuevoAtaud = new ATAUD();
+            using (var r = new Repository<ATAUD>())
+            {
+                nuevoAtaud = r.Create(Ataud);
+            }
+            return nuevoAtaud;
+        }
+
+        public ATAUD ObtenerAtaud(int id)
+        {
+            ATAUD Ataud= null;
+            using (var r = new Repository<ATAUD>())
+            {
+                Ataud = r.Retrieve(p => p.ID == id);
+            }
+            return Ataud;
+        }
+
+        public List<ATAUD> ObtenerAtaudPorNombre()
+        {
+            List<ATAUD> Ataudes = null;
+            using (var r = new Repository<ATAUD>())
+            {
+                Ataudes = r.RetrieveAllOrder(p => p.Codigo);
+            }
+            return Ataudes;
+        }
+
+        public bool Actualizar(ATAUD Ataud)
+        {
+            bool result = false;
+            using (var r = new Repository<ATAUD>())
+            {
+                result = r.Update(Ataud);
+            }
+            return result;
+        }
+
+        public bool Eliminar(ATAUD Ataud)
+        {
+            bool result = false;
+            using (var r = new Repository<ATAUD>())
+            {
+                result = r.Delete(Ataud);
+            }
+            return result;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,103 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AgregarAtaud();
+            //BuscarYModificar();
+            //Listar();
+            BuscarYEliminar();
+            //TipoDeMov();
+            Console.Write("PRESIONE PARA FINALIZAR");
+            Console.ReadLine();
+        }
+        static void AgregarAtaud()
+        {
+            ATAUD ataud = new ATAUD();
+            {
+                ataud.Codigo = "Barnizado";
+                ataud.Precio = 17000;
+                using (var repos = new Repository<ATAUD>())
+                {
+                    repos.Create(ataud);
+                }
+                Console.WriteLine("ATAUD: {0}", ataud.Codigo);
+                Console.WriteLine("PRECIO: ${0}", ataud.Precio);
+            }
+        }
+
+        static void BuscarYModificar()
+        {
+            using (var repos = new Repository<ATAUD>())
+            {
+                var A = repos.Retrieve(a => a.ID == 2);
+                if (A != null)
+                {
+                    Console.WriteLine(A.Codigo);
+                    A.Codigo = A.Codigo + "***";
+                    repos.Update(A);
+                    Console.WriteLine("NOMBRRE MODIFICADO");
+                }
+                else
+                {
+                    Console.WriteLine("ATAUD NO ENCONTRADO");
+                }
+            }
+        }
+
+        static void Listar()
+        {
+            using(var repos = new Repository<ATAUD>())
+            {
+                var ATAUD = repos.Filter(a => a.ID == 1);
+                foreach(var a in ATAUD)
+                {
+                    Console.WriteLine("{0}", a.Codigo);
+                }
+            }
+        }
+
+        static void BuscarYEliminar()
+        {
+            using (var repos = new Repository<ATAUD>())
+            {
+                var A = repos.Retrieve(a => a.ID != 1);
+                if (A != null)
+                {
+                    Console.WriteLine(A.Codigo);
+                    repos.Delete(A);
+                    Console.WriteLine("ATAUD ELIMINADO");
+                }
+                else
+                {
+                    Console.WriteLine("ATAUD NO ENCONTRADO");
+                }
+            }
+        }
+
+        static void TipoDeMov()
+        {
+            MOV mov = new MOV();
+            {
+                mov.Fecha = DateTime.Now;
+                mov.Proveedor = "ETERNITY";
+                //mov.TMov = bool;
+                using (var repos = new Repository<MOV>())
+                {
+                    repos.Create(mov);
+                }
+                Console.WriteLine("FECHA: {0}", mov.Fecha);
+                Console.WriteLine("PROVEEDOR: {0}", mov.Proveedor);
+            }
+        }
+    }
+}
